06307f2f1be7018523cb9e0e7f851d310c35c96a
Pierce <pierceh.dev@gmail.com>
2018-10-15 09:22:43 -0500

    add license

--- mix.exs
+++ mix.exs
@@ -7,6 +7,10 @@ defmodule StrongMap.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      name: "StrongMap",
+      source_url: "https://github.com/pierce-h/strong_map"
       deps: deps()
     ]
   end
@@ -21,8 +25,21 @@ defmodule StrongMap.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "Converts a map with stringified keys to atoms and filters specified keys."
+  end
+
+  defp package() do
+    [
+      maintainers: ["Pierce Harmon"],
+      # This option is only needed when you don't want to use the OTP application name
+      name: "StrongMap",
+      # These are the default files included in the package
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/pierce-h/strong_map"}
     ]
   end
 end
