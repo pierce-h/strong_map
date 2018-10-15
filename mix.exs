707b69ba853166c656616777befbdfc1cccaa617
Pierce <pierceh.dev@gmail.com>
2018-10-15 09:26:05 -0500

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule StrongMap.MixProject do
     [
       app: :strong_map,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
